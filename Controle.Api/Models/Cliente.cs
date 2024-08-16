129242fa8f6b4c96152597db8f046fb71335d96e
Leandro Zanatta <leandrozanatta7@gmail.com>
2024-08-16 17:53:08 -0300

    Adiciona entidades e controlador Clientes

---
+++ Controle.Api/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace Controle.Api.Models;
+public class Cliente
+{
+    public int Id{ get; set; }
+    public string? Nome { get; set; }
+    public string? Documento { get; set; }
+    public string? Telefone { get; set; }
+    public string? Endereco { get; set; }
+}
---
+++ Controle.Api/Models/Cobranca.cs
@@ -0,0 +1,9 @@
+﻿namespace Controle.Api.Models;
+public class Cobranca
+{
+    public int Id{ get; set; }
+    public string? Descricao { get; set; }
+    public decimal Valor{ get; set; }
+    public DateTime DataVencimento { get; set; }
+    public int Pago { get; set; }
+}
