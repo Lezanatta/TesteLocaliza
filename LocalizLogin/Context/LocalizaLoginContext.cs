632c0ff9000cf4c4fa7f78156176c3b587434b1f
Leandro Zanatta <leandrozanatta7@gmail.com>
2024-08-16 23:18:56 -0300

    Adiciona api para autenticacao

---
+++ LocalizLogin/Context/LocalizaLoginContext.cs
@@ -0,0 +1,15 @@
+﻿using LocalizLogin.EntityConfigurations;
+using LocalizLogin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalizLogin.Context;
+public class LocalizaLoginContext(DbContextOptions<LocalizaLoginContext> options) : DbContext(options)
+{
+    public DbSet<Usuario> Usuario { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ LocalizLogin/EntityConfigurations/UsuarioConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using LocalizLogin.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocalizLogin.EntityConfigurations;
+public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+{
+    public void Configure(EntityTypeBuilder<Usuario> builder)
+    {
+        builder.Property(c => c.Id).HasColumnName("idUsuario");
+        builder.Property(c => c.Nome).HasColumnName("Nome");
+        builder.Property(c => c.Email).HasColumnName("Email");
+        builder.Property(c => c.Senha).HasColumnName("Senha");
+
+        builder.HasKey(c => c.Id);
+    }
+}
